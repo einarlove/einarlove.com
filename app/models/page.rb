ba4ad29679d7a0d1de5b08d141e6a7f0635404af
Einar Love <einar.love@gmail.com>
2012-05-20 18:16:46 +0200

    Added validation for pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,10 @@
 class Page < ActiveRecord::Base
   attr_accessible :body, :image, :name, :title, :layout, :visible, :slug, :description
 
+  validates :name,   presence: true
+	validates :layout, presence: true
+  validates :slug,   presence: true, uniqueness: true
+  
   extend FriendlyId
   friendly_id :name, use: :slugged
 end
