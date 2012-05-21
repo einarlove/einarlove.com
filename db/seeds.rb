0295b77d6311e672ebcb4e7a1b4aba513ff9c961
Einar Love <einar.love@gmail.com>
2012-05-21 14:20:19 +0200

    once more fixed seed, and made portfolio items simple_format

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ pages = Page.create([
 	{name: "contact",
 	title: "Contact information",
 	description: "How to get in touch",
-	body: "If you have a project or request for a job that you want collaboration on, you can contact me with the form on the right. 
+	body: "If you have a project or request for a job that you want collaboration on, you can contact me with the form on the right.
+
 I am also reachable on twitter as <a href='http://twitter.com/einarlove'>@einarlove</a>, or at <a href='http://github.com/einarlove'>Github</a>.",
 	layout: "contact",
 	visible: true},
@@ -19,7 +20,9 @@ I am also reachable on twitter as <a href='http://twitter.com/einarlove'>@einarl
 	title: "A short introduction",
 	description: "About me",
 	body: "My name is Einar Ágúst Löve, born in Iceland, raised in Norway but lived on the internet once it got its grip on me.
+
 		Studied media and communication in gymnas where I experienced film production, animation, graphic design, web design and illustration.
+
 		The power to create your own custom worlds on the internet which benefited from imagination and empathy really attracted me. So after gymnas I attended Interaction design at the Norwegian school of creativity in Oslo where I reside and study at this date.",
 	layout: "static",
 	image: "me.jpg",
@@ -40,9 +43,10 @@ I am also reachable on twitter as <a href='http://twitter.com/einarlove'>@einarl
 portfolioItems = PortfolioItem.create([
 	{
 		title: "Synøve Dyrkorn",
-		body: "<p>
-		The Norwegian artist Synøve Dyrkorn was in need for a website for her artworks.</p>
-		<p>I was contacted by the chosen designer which needed me to take the work for the frontend code, markup and construct a gallery view.</p>",
+		body: "
+		The Norwegian artist Synøve Dyrkorn was in need for a website for her artworks.
+
+		I was contacted by the chosen designer which needed me to take the work for the frontend code, markup and construct a gallery view.",
 		image: "synovedyrkorn.jpg",
 		involvement: ["Frontend", "Image gallery"],
 		links: ["http://www.sdyrkorn.no"],
