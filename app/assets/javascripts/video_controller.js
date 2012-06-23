84df0cabe760a9d7d4122939e7587b088eae4833
Einar Love <einar.love@gmail.com>
2012-06-23 02:39:25 +0200

    Progress bar for video

--- app/assets/javascripts/video_controller.js
+++ app/assets/javascripts/video_controller.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 	video_list = document.querySelectorAll("video");
 	for(var i=0; i<video_list.length;i++){
-		video = video_list[i];
-		container = video.parentNode
+		var video = video_list[i],
+				container = video.parentNode,
+				timeBar = container.querySelectorAll(".time_bar")[0];
 		container.addEventListener("click", function() {
 			document.body.classList.toggle("playing");
 			if(video.paused)
@@ -14,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
 		video.addEventListener("ended", function() {
 			document.body.classList.remove("playing");
 			container.classList.add("finished");
+		});
+		video.addEventListener("timeupdate", function(){
+			timeBarPercent = 100 * video.currentTime / video.duration;
+			timeBar.style.width = timeBarPercent + "%";
 		});
 	}
 });
