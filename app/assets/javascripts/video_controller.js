7a02c71745ac12debd107f0198eb6c53a7c886ee
Einar Love <einar.love@gmail.com>
2012-06-27 12:04:39 +0200

    Improved videoplayer

--- app/assets/javascripts/video_controller.js
+++ app/assets/javascripts/video_controller.js
@@ -1,34 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
-	video_list = document.querySelectorAll("video");
-	for(var i=0; i<video_list.length;i++){
-		var video = video_list[i],
+	videoList = document.querySelectorAll("video");
+	for(var i=0; i<videoList.length;i++){
+		var video     = videoList[i],
 				container = video.parentNode,
-				timeBar = container.querySelectorAll(".time_bar")[0];
-		container.addEventListener("click", function() {
-			if(navigator.userAgent.indexOf("MSIE") > 1){
-				document.body.className = document.body.className=="playing" ? null : "playing";
-				console.log(document.body.className);
-			}
-			else
-				document.body.classList.toggle("playing");
-			if(video.paused)
+				timeBar   = container.querySelectorAll(".time_bar")[0],
+				repeatCheck, videoIsReady;
+
+		container.addEventListener("click", function(){videoClick()});
+		video.addEventListener("ended", function(){videoEnd()});
+		video.addEventListener("timeupdate", function(){updateTimer()});
+		video.addEventListener("canplaythrough", function(){videoReady()});
+
+		function videoClick() {
+			if(video.paused){
 				video.play();
+				if(videoIsReady){
+					document.body.classList.add("playing");
+				}
+				else{
+					video.pause();
+					container.classList.add("buffering");
+					repeatCheck = setInterval(checkLoading, 1000);
+				}
+			}
 			else{
+				document.body.classList.remove("playing");
 				video.pause();
 				video.currentTime = 0;
 			}
-			if(video.buffered.length == 0 && document.body.className=="playing"){
-				setTimeout('document.body.classList.add("buffering")', 1000);
+		};
+		function checkLoading(){
+			if(videoIsReady){
+				container.classList.remove("buffering");
+				document.body.classList.add("playing");
+				clearInterval(repeatCheck);
+				video.play();
 			}
-		});
-		video.addEventListener("ended", function() {
-			document.body.classList.remove("playing");
-			container.classList.add("finished");
-		});
-		video.addEventListener("timeupdate", function(){
+		};
+		function videoReady(){
+			videoIsReady = true;
+		};
+		function updateTimer(){
 			timeBarPercent = 100 * video.currentTime / video.duration;
 			timeBar.style.width = timeBarPercent + "%";
-			document.body.classList.remove("buffering");
-		});
+		};
+		function videoEnd() {
+			document.body.classList.remove("playing");
+			container.classList.add("finished");
+		};
 	}
 });
+
+// Add classlist support for Internet Explorer
+if(navigator.userAgent.indexOf("MSIE") > -1){
+	script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "/assets/classlist_shim.js";
+	document.getElementsByTagName("head")[0].appendChild(script);
+};
